cd0ed58c494cda181dda69701005aa481c7b88c9
christycui <christycui14@gmail.com>
2015-03-03 12:29:07 -0500

    removed turbolinks for jquery effects

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ gem 'bootstrap-sass', '~> 3.3.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
-gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 
 # Use ActiveModel has_secure_password
--- app/assets/javascripts/toggle_edit.js
+++ app/assets/javascripts/toggle_edit.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#edit').click(function() {
+  $('#edit').on('click', function() {
     $(".edit_group").toggle();
     $(this).text(function(i, text){
       return text === "Edit Watchlist" ? "Hide Edits" : "Edit Watchlist";
