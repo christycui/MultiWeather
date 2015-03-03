bf0f6232bec48e68dfc3fe9348f7f0ce115cdebd
christycui <christycui14@gmail.com>
2015-03-03 01:28:00 -0500

    changed gem file

--- db/migrate/20150227210738_create_locations.rb
+++ db/migrate/20150227210738_create_locations.rb
@@ -2,7 +2,7 @@ class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.integer :user_id, :city_id
-      t.string :name, :country
+      t.string :name, :country, :lat, :lon
     end
   end
 end
