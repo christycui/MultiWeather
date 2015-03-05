003625fa3d76bde0a0e753989091433b27e15f20
christycui <christycui14@gmail.com>
2015-03-05 07:28:35 -0500

    another refinement on search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,9 +12,9 @@ module ApplicationHelper
   def name_lookup(city_id)
     source = Nokogiri::XML(open("http://api.geonames.org/get?geonameId=#{city_id}&username=christy931"))
     if source.at_xpath('geoname//adminCode1').nil?
-      source.at_xpath('geoname//name').text + ', ' + source.at_xpath('geoname//countryCode').text
+      source.at_xpath('geoname//toponymName').text + ', ' + source.at_xpath('geoname//countryCode').text
     else
-      source.at_xpath('geoname//name').text + ', ' + (source.at_xpath('geoname//adminCode1').text.to_i == 0 ? source.at_xpath('geoname//adminCode1').text + ', ' : '') + source.at_xpath('geoname//countryCode').text
+      source.at_xpath('geoname//toponymName').text + ', ' + (source.at_xpath('geoname//adminCode1').text.to_i == 0 ? source.at_xpath('geoname//adminCode1').text + ', ' : '') + source.at_xpath('geoname//countryCode').text
     end
   end
   
